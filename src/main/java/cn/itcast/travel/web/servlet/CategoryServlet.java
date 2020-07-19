e2ca0c031645b25fba9a74d91fba2e3124165d98
longteng <1021061626@qq.com>
2020-07-20 03:40:36 +0800

    创建分类数据展示CategoryServlet

---
+++ src/main/java/cn/itcast/travel/web/servlet/CategoryServlet.java
@@ -0,0 +1,11 @@
+package cn.itcast.travel.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * 分类数据展示servlet
+ * @author longteng
+ */
+@WebServlet("/Category/*")
+public class CategoryServlet extends BaseServlet {
+}
